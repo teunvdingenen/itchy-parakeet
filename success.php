758fb93bd5c657ce259467ad60e98e39fae5eb94
Teun van Dingenen <teunvdingenen@gmail.com>
2016-04-21 11:40:45 +0200

    fixed css links

--- success.php
+++ success.php
@@ -84,7 +84,7 @@ if( $signup === FALSE ) {
     <!-- <link href="../../assets/css/ie10-viewport-bug-workaround.css" rel="stylesheet"> -->
 
     <!-- Custom styles for this template -->
-    <link href="main.css" rel="stylesheet">
+    <link href="css/main.css" rel="stylesheet">
 
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!--[if lt IE 9]>
