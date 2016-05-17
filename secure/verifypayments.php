9bbc179c5a66f7015354d9d6e2b7112417b35f42
Teun van Dingenen <teunvdingenen@gmail.com>
2016-05-17 10:25:22 +0200

    added payment verification search

---
+++ secure/verifypayments.php
@@ -0,0 +1,130 @@
+<?php session_start();
+include "../functions.php";
+include "createmenu.php";
+
+if(!isset($_SESSION['loginuser'])) {
+    header('Location: ../login');
+}
+$menu_html = "";
+$user_info = get_user_info($_SESSION['loginuser']);
+$user_info_name = $user_info[$db_user_name];
+$user_info_permissions = $user_info[$db_user_permissions];
+
+$menu_html = get_menu_html();
+$returnVal = "";
+$infoBlock = "";
+
+try {
+    include "../mollie_api_init.php";
+} catch (Mollie_API_Exception $e) {
+    addError("Er is iets fout gegaan met de Mollie link");
+    _exit();
+}
+
+$payments = $mollie->payments->all();
+$mysqli = new mysqli($db_host, $db_user, $db_pass, $db_name);
+
+for($i = 0; $i < $payments['totalCount']; $i++ ) {
+    $payment = $payments['data'][$i];
+    if( !$payment->isPaid() ) {
+        continue;
+    }
+    $query = sprintf("SELECT * FROM buyer WHERE id = '%s'", $mysqli->real_escape_string($payment["id"]));
+    $sqlresult = $mysqli->query($query);
+    if( !$sqlresult ) {
+        addError("Unable to query database");
+    } else if ($sqlresult->num_rows == 1) {
+        addInfo(sprintf("No payment in database for id: %s, code: %s.", $payment["id"], $payment["metadata"]["raffle"]));
+    } else {
+        $row = $sqlresult->fetch_array(MYSQLI_ASSOC);
+        if( $row['complete'] != 1 ) {
+            addInfo(sprintf("Payment with id: %s, code %s is not marked as payed.", $row['id'], $row['code']));
+        }
+    }
+}
+
+function addError($value) {
+    global $returnVal;
+    $returnVal .= "<div class='alert alert-danger'>".$value."</div>";
+}
+
+function addInfo($value) {
+    global $infoBlock;
+    $infoBlock .= "<div class='alert alert-info'>".$value."</div>";
+}
+?>
+
+<!doctype html>
+<html class="no-js" lang="">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+        <meta name="description" content="">
+        <meta name="author" content="Teun van Dingenen">
+        <link rel="icon" href="../favicon.ico">
+
+        <title>Familiar Forest Dashboard</title>
+
+        <!-- Bootstrap core CSS -->
+        <link href="../css/bootstrap.min.css" rel="stylesheet">
+
+        <!-- Custom styles for this template -->
+        <link href="../css/main.css" rel="stylesheet">
+
+        <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+        <!--[if lt IE 9]>
+          <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+          <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+        <![endif]-->
+    </head>
+    <body>
+        <!--[if lt IE 8]>
+            <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+
+        <!-- Add your site or application content here -->
+        <nav class="navbar navbar-inverse navbar-fixed-top">
+          <div class="container-fluid">
+            <div class="navbar-header">
+              <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+              </button>
+              <a class="navbar-brand" href="#">Familiar Forest Festival</a>
+            </div>
+            <div id="navbar" class="navbar-collapse collapse">
+              <ul class="nav navbar-nav navbar-right">
+                <li><a class='menulink' href='logout.php'>Logout</a></li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-sm-3 col-md-2 sidebar">
+                  <?php echo $menu_html ?>
+                </div>
+            </div>
+            <div id="content" class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
+                <?=$returnVal?>
+                <?=$returnVal?>
+            </div>
+        </div>
+
+    <!-- Bootstrap core JavaScript
+        ================================================== -->
+        <!-- Placed at the end of the document so the pages load faster -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+        <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
+        <script src="../js/vendor/bootstrap.min.js"></script>
+
+        <script src="../js/plugins.js"></script>
+        <script src="../js/main.js"></script>
+        <script src="js/secure.js"></script>
+    </body>
+</html>
